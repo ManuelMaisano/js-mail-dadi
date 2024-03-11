6be0cca4c81f3244a075df74368a7bce90dd1e78
ManuelMaisano <manumaisano@gmail.com>
2024-03-11 22:07:34 +0100

    add ciclo for

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const userEmail = prompt('Scrivi qui la tua email per accedere')
 // lista di email 
 const listEmailUsers = ['manumaisano@gmail.com', 'booleansrl@gmail.com', 'mirkomaisano@gmail.com', 'elsavecchione@gmail.com', 'anonimoutente@gmail.com'];
 
+let emailFound = false; 
+for (let i = 0; i < listEmailUsers.length; i++) { 
+    const login = listEmailUsers[i]
+
+    if (userEmail === login) { 
+
+        emailFound = true; 
+    }
+};
+
+if (emailFound === true) { 
+    alert('La mail è presente puoi accedere')
+} else {
+    alert('Mi dispiace, non puoi accedere') 
+};
