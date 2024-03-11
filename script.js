fd0381978d0e515998e690600b46512c516d57f7
ManuelMaisano <manumaisano@gmail.com>
2024-03-11 22:06:20 +0100

    add array and const

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert('ciao')
+
+// Chiediamo all'utente la sua email 
+const userEmail = prompt('Scrivi qui la tua email per accedere')
+
+// lista di email 
+const listEmailUsers = ['manumaisano@gmail.com', 'booleansrl@gmail.com', 'mirkomaisano@gmail.com', 'elsavecchione@gmail.com', 'anonimoutente@gmail.com'];
+
